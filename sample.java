ca5586e0684367e779c6d0916fd5f1005e340339
Satish Dhanakotti <53414430+satishdhanakotti@users.noreply.github.com>
2021-09-09 14:34:58 +0530

    Delete sample.java

--- sample.java
+++
@@ -1,7 +0,0 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");
-     System.out.println("Hello Java1");
-     System.out.println("Hello Java2");    
-    }  
-}  
